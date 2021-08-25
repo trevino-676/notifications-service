6c50968ff6bdf165d5cdcf82ad137c0cd0faf091
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-08-25 00:08:31 -0500

    Se agregaron los metodos para buscar los usuarios desde las companias y los proveedores

--- service.py
+++ service.py
@@ -15,7 +15,34 @@ collection = db["notifications"]
 # ssl_context.load_verify_locations(pem_file)
 
 
+def __convert_supplier_to_user(notification: dict):
+    if "suppliers" not in notification:
+        return notification
+
+    filters = {"suppliers": {"$in": notification["suppliers"]}}
+    companies = db["companies"].find(filters)
+    companies_rfc = [item["rfc"] for item in list(companies)]
+    company_filters = {"companies": {"$in": companies_rfc}}
+    users = db["users"].find(company_filters)
+    notification["users"] = [str(item["_id"]) for item in list(users)]
+    notification.pop("suppliers")
+    return notification
+
+
+def __convert_company_to_user(notification: dict):
+    if "companies" not in notification:
+        return notification
+
+    filters = {"companies": {"$in": notification["companies"]}}
+    users = db["users"].find(filters)
+    notification["users"] = [str(item["_id"]) for item in list(users)]
+    notification.pop("companies")
+    return notification
+
+
 def cretate_notification(notification: dict) -> dict:
+    notification = __convert_supplier_to_user(notification)
+    notification = __convert_company_to_user(notification)
     new_notification = collection.insert_one(notification)
     created_notification = collection.find_one({"_id": new_notification.inserted_id})
     created_notification["_id"] = str(created_notification["_id"])
